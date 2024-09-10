20432bc18bf472e7e2159463f7445a97684baf9e
Johannes <jfi@ssb.no>
2024-09-10 16:04:06 +0200

    The request urls "for the execute" buttos are missing the virtual paths. (The gui and and swagger.json appeared)

--- PxWeb/Program.cs
+++ PxWeb/Program.cs
@@ -158,9 +158,9 @@ namespace PxWeb
                 app.UseSwaggerUI(options =>
                 {
                     //options.SwaggerEndpoint("../v2/swagger.json", "PxWebApi 2.0-beta jfi");
-                    options.SwaggerEndpoint("./swagger/v2/swagger.json", "PxWebApi 2.0-beta jfi");
+                    options.SwaggerEndpoint("../swagger/v2/swagger.json", "PxWebApi 2.0-beta jfi");
                     //                    options.SwaggerEndpoint("../swagger/v2/swagger.json", "PxWebApi 2.0-beta jfi");
-                    options.RoutePrefix = string.Empty;
+                    //options.RoutePrefix = string.Empty;
                 });
             }
 
